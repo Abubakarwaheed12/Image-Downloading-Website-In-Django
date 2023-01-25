770bddf316ce904b43134190246c904a0bc6964c
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-01-25 12:33:11 +0500

    image uploader website

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    photo=models.ImageField(upload_to='imgs')
+    date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return 'image'
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .forms import ImageForm
+from django.contrib import messages 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form=ImageForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'your image added successfullly')
+            return redirect('home')
+    else:      
+        f=ImageForm()
+        return render(request, 'index.html' ,{'form':f})
